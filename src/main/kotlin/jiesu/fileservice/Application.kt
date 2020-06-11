3f4a59f02d9eedbf3489c0e59d7d54ecd4feb1ed
Jie Su <jiesu12@gmail.com>
2020-06-10 23:31:47 -0500

    become a discovery client

--- src/main/kotlin/jiesu/fileservice/Application.kt
+++ src/main/kotlin/jiesu/fileservice/Application.kt
@@ -2,8 +2,10 @@ package jiesu.fileservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class Application
 
 fun main(args: Array<String>) {
